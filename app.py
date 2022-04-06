6def21292770a7097dead62a54af825bb2b41c1b
Sanrobson <sanrobson.alves@gmail.com>
2022-04-06 15:33:43 -0300

    estudo python

---
+++ app.py
@@ -0,0 +1,15 @@
+# Nome = input("Digite o nome do cliente: ")
+# print("Olá,", Nome, "Como vc esta ?")
+
+# nome= input("qual seu nome:")
+# idade =input("entre com sua idade :")
+# mensagem =("Parabens,")
+# mensagem2=("Vc esta com ")
+# print (mensagem,nome,mensagem2,idade)
+
+# age = 23
+# message = "Happy " + str(age) + "rd Birthday!"
+# print(message)
+# estoque = ['arroz',
+#            'Feijao', 'açucar', 'milho']
+# print(estoque[2].title()) #o .title() escreve em maiusculo
