572ff3c4c86936b8e9e8111ae5ee652d29380aa1
Sanrobson <sanrobson.alves@gmail.com>
2022-04-19 15:10:30 -0300

    curso_intesivo_python_exercicio_3-2

---
+++ cap3-2.py
@@ -0,0 +1,6 @@
+nomes = ['ana', 'felipe', 'wesley']
+message0 = "Olá " + nomes[0] + " tudo bem contigo, quanto tempo?"
+message1 = "Olá " + nomes[1] + " tudo bem contigo, iai beleza?"
+message2 = "Olá " + nomes[2] + " tudo bem contigo, opa tudo em ordem?"
+
+print(" \n " + message0 + " \n " + message1 + " \n " + message2)
