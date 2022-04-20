4713284bbb37e89ef986195278a892ef0b81dac7
Sanrobson <sanrobson.alves@gmail.com>
2022-04-20 14:24:02 -0300

    curso_intesivo_python_exercicio_3-3

---
+++ cap3-3.py
@@ -0,0 +1,8 @@
+Transportes = ['Carro', 'Moto', 'Metro']
+message0 = "Olá Amigo hoje vamos de " + Transportes[0] + "."
+message1 = "Olá amigo quer ir de  " + Transportes[1] + " ?"
+message2 = "Ferrrou vamos de  " + Transportes[2] + " hoje."
+
+
+print(" \n " + message0 + " \n " + message1 +
+      " \n " + message2)
