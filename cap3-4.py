a97e743663422695cf45a997849c1d7a41115a99
Sanrobson <sanrobson.alves@gmail.com>
2022-04-21 16:27:45 -0300

    Exercicio_3-4

---
+++ cap3-4.py
@@ -0,0 +1,8 @@
+jantar = ['Ro', 'Mauro', 'Marcos']
+message0 = "Olá Amigo hoje tem salada vc gosta " + jantar[0] + "?"
+message1 = "Tem carne e peixe oque vc vai querer " + jantar[1] + " ?"
+message2 = "Deixa espaço para sobremesa " + jantar[2] + "."
+
+print(" \n " + message0 + " \n " + message1 +
+      " \n " + message2)
+print(jantar)
