69e3ffb0370ab5e7d243c4235eff9abf8a059d84
Sanrobson <sanrobson.alves@gmail.com>
2022-04-21 16:59:49 -0300

    Lista de Exercicios resolvidos.

---
+++ cap3-5.py
@@ -0,0 +1,12 @@
+# Solução do Problema referente ao Capítulo 3, questão 3.5
+
+jantar = ['Ro', 'Mauro', 'Marcos']
+print("Ola a todos,Ro nào podera comparecer ao jantar. ")
+jantar[0] = ("Carlos")
+
+message0 = "Olá Amigo hoje tem salada vc gosta " + jantar[0] + "?"
+message1 = "Tem carne e peixe oque vc vai querer " + jantar[1] + " ?"
+message2 = "Deixa espaço para sobremesa " + jantar[2] + "."
+
+print(" \n " + message0 + " \n " + message1 +
+      " \n " + message2)
