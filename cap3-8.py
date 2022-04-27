f49dbc37844e2e8b22999980f2cf0951379a3735
Sanrobson <sanrobson.alves@gmail.com>
2022-04-27 17:03:22 -0300

    Exercicio_3-8

---
+++ cap3-8.py
@@ -0,0 +1,14 @@
+# Solução do Problema referente ao Capitulo 3, questao 3.8
+
+lugares = ['Paraguai', 'Italia', 'Japão', 'França', 'China']
+print(lugares)
+print(sorted(lugares))
+print(lugares)
+lugares.reverse()
+print(lugares)
+lugares.reverse()
+print(lugares)
+lugares.sort()
+print(lugares)
+lugares.sort(reverse=True)
+print(lugares)
