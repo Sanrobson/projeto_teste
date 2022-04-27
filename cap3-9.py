931759bd0f12b609c52a435889b84406eca7738e
Sanrobson <sanrobson.alves@gmail.com>
2022-04-27 17:09:21 -0300

    Exercicio_3-9

---
+++ cap3-9.py
@@ -0,0 +1,5 @@
+# Solução do Problema referente ao Capitulo 3, questao 3.9
+pessoas = ['Ricardo', 'Tiago','Victor', 'Lucas']
+print(len(pessoas))
+lugares = ['Paraguai', 'Italia', 'Japão', 'França', 'China']
+print(len(lugares))
