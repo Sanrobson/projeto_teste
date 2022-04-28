2f7e9f39f893e7c36210afab06383b6ac2ce91fa
Robson alves <102808100+Sanrobson@users.noreply.github.com>
2022-04-28 14:30:46 -0300

    Create cap4_4.1.py
    exercício CAP 4

---
+++ Capítulo 4/cap4_4.1.py
@@ -0,0 +1,5 @@
+pizzas  = [ 'portuguesa' , 'frango com catupiry' , '4 queijos' ]
+
+para  pizza  em  pizzas :
+    print ( "Hmm, gosto de pizza"  +  pizza . titulo ())
+print ( "Eu realmente adoro pizza!" )
